d1bd84e0d2478994c5b51a741c32eeee62558b4c
ramazanerikli <ramazanerikli1@gmail.com>
2022-11-15 20:49:08 +0300

    Mail api fix

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -54,7 +54,7 @@ export default function Contact() {
               id="name"
               name="name"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Flow Bite"
+              placeholder=""
               value={name}
               onChange={(e) => setName(e.target.value)}
               required
@@ -72,7 +72,6 @@ export default function Contact() {
               id="email"
               name="email"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@flowbite.com"
               pattern=".+@.+\..+"
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -90,7 +89,6 @@ export default function Contact() {
               name="message"
               rows={4}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Your message..."
               value={message}
               onChange={(e) => setMessage(e.target.value)}
             ></textarea>
