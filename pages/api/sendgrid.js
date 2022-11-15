2dec1950da2f565767e299094905bf37e3fe4602
ramazanerikli <ramazanerikli1@gmail.com>
2022-11-15 17:22:04 +0300

    Vercel environment variables

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -4,7 +4,6 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendEmail(req, res) {
   try {
-    // console.log("REQ.BODY", req.body);
     await sendgrid.send({
       to: "ramazanerikli1@gmail.com", // Your email where you'll receive emails
       from: "ramazanerikli1@gmail.com", // your website email address here
